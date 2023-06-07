867f149ba01b8bbc564332d0456cf30136bfd24d
Alessandro <alessandro.zanni87@gmail.com>
2023-06-07 13:54:30 +0200

    added headless

--- Dockerfile
+++ Dockerfile
@@ -3,22 +3,15 @@ FROM python:3.10
 COPY . /app
 WORKDIR /app
 
-#RUN printf "deb  http://ch.archive.ubuntu.com/ubuntu/ saucy main restricted\ndeb-src  http://ch.archive.ubuntu.com/ubuntu/ saucy main restricted" > /etc/apt/sources.list.d/backports.list
-#RUN echo 'deb http://deb.debian.org/debian jessie-backports main' > /etc/apt/sources.list.d/backports.list
-
 RUN apt-get update
-RUN apt -f install -y
-#RUN apt-get build-dep build-essential
-#RUN apt-get install -y libxss1 libappindicator1 
-#libindicator7
 RUN wget -q https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 RUN apt-get install -y ./google-chrome-stable_current_amd64.deb
 
-COPY chromedriver /usr/local/bin/chromedriver
-RUN chmod 777 /usr/local/bin/chromedriver
+COPY chromedriver /usr/bin/chromedriver
+RUN chmod 777 /usr/bin/chromedriver
 
-# chromium-browser
-#RUN pip install --upgrade pip
+RUN chmod 777 /usr/bin/google-chrome
+
 RUN pip install -r requirements.txt
 
-CMD [ "python3", "stack_overflow_page.py" ]
+#CMD [ "python3", "stack_overflow_page.py" ]
--- stack_overflow_page.py
+++ stack_overflow_page.py
@@ -1,14 +1,22 @@
-import os,logging
+import os
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 def login():
-    logging.info("Logging into stackoverflow.com")
+    print("Logging into stackoverflow.com")
 
-    driver = webdriver.Chrome()
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
     try:
         driver.get("https://stackoverflow.com")
 
@@ -18,12 +26,12 @@ def login():
         driver.find_element("id", "password").send_keys(os.environ['STACK_OVERFLOW_PASSWORD'])
         driver.find_element("id", "submit-button").submit()
 
-        driver.find_element(By.PARTIAL_LINK_TEXT, os.environ.get('STACK_OVERFLOW_DISPLAY_NAME')).click()
+#        driver.find_element("link text", os.environ['STACK_OVERFLOW_DISPLAY_NAME']).click()
 
         elem = WebDriverWait(driver, 5).until(
             expected_conditions.presence_of_element_located((By.ID, "js-daily-access-calendar-container"))
         )
-        logging.info("Logged into stackoverflow.com and accessed profile page - " + elem.text)
+        print("Logged into stackoverflow.com and accessed profile page - " + elem.text)
 
     except Exception as e:
         print("An error occurred while trying to access stackoverflow.com!", e)
